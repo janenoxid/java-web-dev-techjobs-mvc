a1dddfec11382e79305376ca7a4ce229a957d245
janenoxid <desireejanedixon@gmail.com>
2021-02-28 11:00:20 -0600

    Got the search functionality to work. Just need to do the sanity checks.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,4 +25,21 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+
+        ArrayList<Job> jobs;
+        if (searchTerm.equalsIgnoreCase("all") ||
+            searchTerm.equals("")){
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("searchType", searchType);    // commenting these out since I'm trying out the @ModelAttribute
+        model.addAttribute("searchTerm", searchTerm);    // instead of @RequestParam... I think that's right? Not sure though.
+        return "search";
+    }
+
 }
